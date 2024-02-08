7ac0fffa5fc6245069b59f02257a5131233044f6
Chanchal Sharma <85624223+champdoescode@users.noreply.github.com>
2024-02-08 17:17:26 +0530

    Add files via upload

---
+++ TRAVEL AND TOURISM/JS/contactUs.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById("contactUs").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("contactUs").style.display = "none";
+  }
